0e9a0afeef7b3cfb6c78f9c26c33cdd707907f4e
mrtambour <6724649+mrtambour@users.noreply.github.com>
2023-02-14 00:42:32 -0800

    code refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 use std::{env, fs, io};
 
-fn main() {
-    let current_path = env::current_dir().expect("error getting current directory");
-    let current_dir = current_path
-        .into_os_string()
-        .into_string()
-        .expect("error converting current path to string");
+fn scan_directory(current_dir: &String) -> Vec<String> {
     let mut archives_list = vec![];
-    for entry in fs::read_dir(&current_dir).expect("error reading current directory") {
+    for entry in fs::read_dir(current_dir).expect("error reading current directory") {
         let entries = entry.expect("error copying entry variable");
         let file_name = entries
             .file_name()
@@ -20,25 +15,43 @@ fn main() {
         }
     }
 
-    let leave_original_file = true;
+    archives_list
+}
+
+fn get_input() -> bool {
+    let mut leave_original_file = true;
+    let mut input = String::new();
+
     println!(
         "Would you like to leave the original file? Y/N /n Currently set to: {}",
-        leave_original_file.to_string() // accept input
+        leave_original_file
     );
 
-    let mut input = String::new();
     match io::stdin().read_line(&mut input) {
-        Ok(n) => {
-            println!("Setting to {} ", input);
+        Ok(_ok) => {
             if input == "N" {
-                *leave_original_file = false;
+                leave_original_file = false;
             }
         }
-        Err(error) => println!("error: {}", error),
+        Err(error) => println!("error: {}", error)
     }
+    leave_original_file
+}
 
-    for archive in archives_list {
-        println!("Processing: {}", archive.to_string());
+fn get_current_directory() -> String {
+    let current_path = env::current_dir().expect("error getting current directory");
+
+    let current_dir = current_path
+        .into_os_string()
+        .into_string()
+        .expect("error converting current path to string");
+
+    current_dir
+}
+
+fn process_zip_files(archive_list: Vec<String>, leave_original_file: bool) {
+    for archive in archive_list {
+        println!("Processing: {}", archive);
         let mut archive_name = archive.to_string();
         let original_file_name = archive.to_string();
         let archive_name_length = archive_name.len();
@@ -46,11 +59,23 @@ fn main() {
         archive_name = format!("{}{}", archive_name, ".cbz");
         println!("Truncated archive name: {}", archive_name);
 
-        if leave_original_file == true {
-            fs::copy(original_file_name, archive_name);
-        } else if leave_original_file == false {
+        if leave_original_file {
+            fs::copy(original_file_name, archive_name).unwrap();
+        } else if !leave_original_file {
             fs::rename(original_file_name, archive_name).expect("error renaming a file");
             println!("deleted original");
         }
+    }
+}
+
+fn main() {
+    let leave_original_file = get_input();
+    let current_directory = get_current_directory();
+    let archive_list = scan_directory(&current_directory);
+
+    if archive_list.is_empty() {
+        println!("no archives detected")
+    } else {
+        process_zip_files(archive_list, leave_original_file);
     }
 }
